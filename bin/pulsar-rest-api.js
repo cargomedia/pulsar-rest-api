8edecb14240d7cfa7342997075c33888fc0ec337
Chris Stasiak <me@krzysztof-stasiak.com>
2014-01-27 15:39:56 +0100

    Redesigned

--- bin/pulsar-rest-api.js
+++ bin/pulsar-rest-api.js
@@ -6,15 +6,15 @@ var fs = require('fs');
 var argv = optimist.argv;
 var logDir = argv['log-dir'];
 
-var sslKey = argv['ssl-key'];
-var sslCert = argv['ssl-cert'];
+var sslKey = argv['ssl-key'] || './bin/ssl/*.pulsar.local.key';
+var sslCert = argv['ssl-cert'] || './bin/ssl/*.pulsar.local.pem';
 var sslPfx = argv['ssl-pfx'];
 var sslPassphrase = argv['ssl-passphrase'];
 
-var authClientId = argv['auth-client-id'];
-var authClientSecret = argv['auth-client-secret'];
+var authUsername = argv['auth-username'] || null;
+var authPassword = argv['auth-password'] || null;
 var authProvider = argv['auth-provider'] || 'github';
-var authMethod = argv['auth-method'] || 'OAuth2';
+var authMethod = argv['auth-method'] || 'user';
 
 
 if (logDir) {
@@ -23,7 +23,40 @@ if (logDir) {
 
 if (!process.send) {
 	argv = optimist.default('port', '8001').argv;
-	pulsarServer = new pulsar.Server(argv['port'], null, authClientId, authClientSecret, authProvider, authMethod);
+
+	var sslOptions = null;
+	if (sslKey && sslCert) {
+		sslOptions = {
+			key: fs.readFileSync(sslKey)
+		};
+
+		var certFile = fs.readFileSync(sslCert).toString();
+		var certs = certFile.match(/(-+BEGIN CERTIFICATE-+[\s\S]+?-+END CERTIFICATE-+)/g);
+		if (certs && certs.length) {
+			sslOptions.cert = certs.shift();
+			if (certs.length) {
+				sslOptions.ca = certs;
+			}
+		} else {
+			sslOptions.cert = certFile;
+		}
+	}
+	if (sslPfx) {
+		sslOptions = {
+			pfx: fs.readFileSync(sslPfx)
+		};
+	}
+	if (sslOptions && sslPassphrase) {
+		sslOptions.passphrase = fs.readFileSync(sslPassphrase).toString().trim();
+	}
+
+	pulsarServer = new pulsar.Server(
+		argv['port'],
+		sslOptions,
+		authUsername,
+		authPassword,
+		authProvider,
+		authMethod);
 
 	process.on('SIGTERM', function() {
 		pulsarServer.cleanup();

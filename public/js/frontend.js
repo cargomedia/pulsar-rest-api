051df21ce859abe67b6879cfa2aed0a3b28e1ce9
Chris Stasiak <me@krzysztof-stasiak.com>
2014-01-30 14:21:27 +0100

    Added web interface skeleton

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,14 +1,51 @@
+function updateTask(task) {
+	var $taskElement = $('#task-' + task.id);
+	if (!$taskElement.length) {
+		// todo: create the element
+	}
 
+	$taskElement.find('.taskId').text(task.id);
+	$taskElement.find('.status').text(task.status);
+	$taskElement.find('.output').html(task.output);
+	return $taskElement;
+}
+
+function renderTasks(taskList) {
+	var taskListContainer = $('.taskList').html('');
+	for (var id in taskList) {
+		var task = taskList[id];
+		// todo: use templates here
+		var $taskElement = $('<div class="task" id="task-' + task.id + '"><div>Task ID: <span class="taskId"></span></div> <div>Status: <span class="status"></span></div> <pre class="output"></pre></div> <button class="kill" onclick="killTask(' + task.id + ')">kill task</button>');
+		taskListContainer.append($taskElement);
+		updateTask(task);
+		observeTask(task);
+	}
+}
+
+function killTask(taskId) {
+	$.post('/task/' + taskId + '/kill');
+}
+
+function observeTask(task) {
+	$.get('/task/' + task.id + '/state', function(result) {
+		if (result.changed) {
+			task = result.task;
+			var taskListContainer = $('.taskList');
+			updateTask(task);
+		}
+
+		if (task.status == 'running') {
+			observeTask(task);
+		}
+	}, 'json');
+}
+
+function refreshTaskList() {
+	$.get('/tasks', function(taskList) {
+		renderTasks(taskList);
+	}, 'json');
+}
 
 $(function() {
-	$.get('/tasks', function(taskList) {
-		var taskListContainer = $('.taskList').html('');
-		for (var id in taskList) {
-			var $taskItem = $('<pre class="task"></pre>');
-			$taskItem.html(taskList[id].output);
-			taskListContainer.append($taskItem);
-		}
-		console.log(data);
-	}, 'json');
-
+	refreshTaskList();
 });

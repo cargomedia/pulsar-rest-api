800807c061f651e7a6070a914d57f756b3e8ce9b
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-10 19:10:34 +0100

    Updated taskList view

--- public/app/views/taskList.js
+++ public/app/views/taskList.js
@@ -4,18 +4,46 @@ var app = app || {};
 	'use strict';
 
 	app.TaskListView = Backbone.View.extend({
-
-		events: {
-			'click #kill-task': 'killTask',
-			'click #toggle-all': 'toggleAllComplete'
-		},
-
 		initialize: function() {
+            this.$count = $('#task-count');
+
+            this.listenTo(this.collection, 'add', this.addOne);
+            this.listenTo(this.collection, 'all', this.render);
+
+            this.observeTasks();
 		},
 
 		render: function() {
-			this.$el.html('hello');
-		}
+            if (this.collection.length) {
+                this.$count.html(this.collection.length);
+                this.$el.show();
+            } else {
+                this.$el.hide();
+            }
+
+			this.addAll();
+		},
+
+        addOne: function(task) {
+            var view = new app.TaskView({ model: task });
+            this.$el.prepend(view.render().el);
+        },
+
+        addAll: function() {
+            this.$el.html('');
+            this.collection.each(this.addOne, this);
+        },
+
+        observeTasks: function() {
+            var self = this;
+            $.get('/tasks/created', function(result) {
+                if (result.changed) {
+                    this.collection.add(result.task);
+                }
+                self.observeTasks();
+            }, 'json');
+        }
+
 	});
 
 })(jQuery);

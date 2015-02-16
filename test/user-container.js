b56fb7e28a4f24d24d09b1a8d4d6c76afd4131a7
vogdb <vozhdb@gmail.com>
2015-02-16 11:55:39 +0300

    Convert UserContainer._userList to Array.

--- test/user-container.js
+++ test/user-container.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var UserContainer = require('../lib/authentication/user-container');
 var assert = require('chai').assert;
 
@@ -10,21 +11,22 @@ describe('tests of garbage collecting of outdated cookies', function() {
     }
   };
 
-  it.only('', function(done) {
+  it('', function(done) {
     //for the sake of the test
     UserContainer._collectorIntervalMs = 100;
     UserContainer._oudatedTimeMs = UserContainer._collectorIntervalMs + 1;
 
     var userContainer = new UserContainer();
     userContainer.saveUser(responseMock, 'token');
-    var cookies = Object.keys(userContainer._userList);
-    assert(cookies.length == 1);
+    assert(_.size(userContainer._userList) == 1);
 
-    var cookie = cookies[0];
+    var user = userContainer._userList[0];
     setTimeout(function() {
-      assert(userContainer.hasUserByCookie(cookie), 'Cookie should be presented');
+      assert(userContainer.hasUserByCookie(user.cookie), 'Cookie should be presented');
+      assert(userContainer.hasUserByToken(user.token), 'Token should be presented');
       setTimeout(function() {
-        assert(!userContainer.hasUserByCookie(cookie), 'Cookie should be deleted');
+        assert(!userContainer.hasUserByCookie(user), 'User should be deleted');
+        assert(!userContainer.hasUserByToken(user), 'User should be deleted');
         done();
       }, (UserContainer._oudatedTimeMs + 2 * UserContainer._collectorIntervalMs));
     }, UserContainer._collectorIntervalMs);

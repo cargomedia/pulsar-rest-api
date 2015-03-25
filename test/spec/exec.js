09b7ac0f514593efe0dfa5111dd16da8462a42ca
vogdb <vozhdb@gmail.com>
2015-03-25 08:36:27 +0300

    Camel case naming consistency.

--- test/spec/exec.js
+++ test/spec/exec.js
@@ -1,7 +1,7 @@
 require('../../lib/errors');
 var PulsarExec = require('../../lib/pulsar/exec');
 var assert = require('chai').assert;
-var jobArgs = require('../data/job-args');
+var jobArgs = require('../data/jobArgs');
 
 describe('Test constructor arguments of PulsarExec', function() {
 

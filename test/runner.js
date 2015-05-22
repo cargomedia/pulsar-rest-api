3003f1b2135226efb0c3fdb378042f9c4864830f
tomaszdurka <tomasz@durka.pl>
2015-05-22 15:23:11 +0200

    Make runner.js return non-zero code when some of the tests fail

--- test/runner.js
+++ test/runner.js
@@ -2,11 +2,18 @@ var cp = require('child_process');
 var join = require('path').join;
 var walk = require('walk');
 var mochaBin = join(__dirname, '..', 'node_modules', '.bin', 'mocha');
+var _ = require('underscore');
 
+var statusList = [];
 var walker = walk.walk(__dirname + '/spec', {followLinks: false});
 walker.on('file', function(root, stat, next) {
   var filepath = root + '/' + stat.name;
-  cp.spawnSync(mochaBin, [filepath], {stdio: 'inherit'});
-
+  var result = cp.spawnSync(mochaBin, [filepath], {stdio: 'inherit'});
+  statusList.push(result.status);
   next();
+});
+walker.on('end', function() {
+  if (!_.every(statusList, 0)) {
+    process.exit(1);
+  }
 });

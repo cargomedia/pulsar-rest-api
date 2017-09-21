07d2becb9e3cd4e2326b72eea22313d8068315c7
tomaszdurka <tomasz@durka.pl>
2017-09-21 15:44:27 +0200

    Wait for mongodb host/port to be ready

--- test/runner.js
+++ test/runner.js
@@ -1,23 +1,35 @@
 var cp = require('child_process');
-var join = require('path').join;
+var path = require('path');
 var walk = require('walk');
-var mochaBin = join(__dirname, '..', 'node_modules', '.bin', 'mocha');
 var _ = require('underscore');
+var waitForPort = require('wait-for-port');
 
-var exitCodes = [];
-var walker = walk.walk(__dirname + '/spec', {followLinks: false});
-walker.on('file', function(root, stat, next) {
-  var filepath = root + '/' + stat.name;
-  var result = cp.spawnSync(mochaBin, [filepath], {stdio: 'inherit'});
-  exitCodes.push(result.status);
-  next();
-});
+var rootDir = path.join(__dirname, '..');
+var mochaBin = rootDir + '/node_modules/.bin/mocha';
 
-walker.on('end', function() {
-  var hasFailures = _.some(exitCodes, function(status) {
-    return status !== 0;
-  });
-  if (hasFailures) {
-    process.exit(1);
+var Config = require(rootDir + '/lib/config');
+var testConfig = new Config(rootDir + '/test/data/configs/config.yaml').asHash();
+
+waitForPort(testConfig.mongodb.host, testConfig.mongodb.port, function(err) {
+  if (err) {
+    throw new Error(err);
   }
+
+  var exitCodes = [];
+  var walker = walk.walk(__dirname + '/spec', {followLinks: false});
+  walker.on('file', function(root, stat, next) {
+    var filepath = root + '/' + stat.name;
+    var result = cp.spawnSync(mochaBin, [filepath], {stdio: 'inherit'});
+    exitCodes.push(result.status);
+    next();
+  });
+
+  walker.on('end', function() {
+    var hasFailures = _.some(exitCodes, function(status) {
+      return status !== 0;
+    });
+    if (hasFailures) {
+      process.exit(1);
+    }
+  });
 });
